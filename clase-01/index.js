d94e0b63361a2fe9a223cce57ea74f8317cb4a3c
Luis Mera <profeluismeradev@gmail.com>
2023-06-26 22:36:56 -0300

    clase 1

---
+++ clase-01/index.js
@@ -0,0 +1,49 @@
+// Tipos de variable var, let y const
+
+// Declarar una variable
+
+let apellido;
+var nombre;
+
+// Asignar una variable
+
+nombre = "Dante";
+console.log(nombre);
+apellido = "Diaz";
+
+nombre = "Juan";
+console.log(nombre);
+// Variable asignada 
+
+//let edad = 23; 
+
+// Const 
+
+// const pais = "Argentina";
+
+// pais = "Chile";
+
+// Camel Case
+let anioNacimiento = 2000; 
+
+// Salida de datos 
+
+// console.log("Hola Mundo");
+
+// console.log("La edad es: " + edad)
+
+// alert("La edad es: " + edad);
+
+// Entrada de datos
+
+// let edad = prompt("Ingrese su edad");
+// alert("La edad ingresada es: " + edad);
+
+// Ejercicio 
+
+let anioDeNacimiento = parseInt(prompt("Ingrese su año de nacimiento"))
+let anioActual = 2023;
+let edadAprox = anioActual - anioDeNacimiento;
+alert("Tu edad aproximada es: " + edadAprox);
+console.log("Tu edad aproximada es: " + edadAprox);
+
