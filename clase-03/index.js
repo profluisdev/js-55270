f23abe22cab65b913af75a255e52baf8a79f8adc
Luis Mera <profeluismeradev@gmail.com>
2023-07-03 21:25:33 -0300

    Clase 3 ciclo for

---
+++ clase-03/index.js
@@ -0,0 +1,53 @@
+// Estructura del ciclo for
+
+// for( let i = 0; i <= 28; i++ ) {
+//     // i++ equivale a i = i + 1
+//     console.log(i);
+// }
+
+// Algoritmo para calcular la tabla de multiplicar de un número
+
+// let ingresarNumero = parseInt(prompt("Ingrese un número"));
+
+// for(let i = 1; i <= 10; i++) {
+//     let resultado = ingresarNumero * i;
+//     console.log(ingresarNumero + " X " + i + " = " + resultado);
+// }
+
+// Algoritmo para dar turno del 1 al 5 a los nombres ingresados.
+
+// for(let i = 1; i <= 5; i++) {
+//     let ingresarNombre = prompt("Ingresar nombre");
+//     alert("Turno N° " + i + " Nombre " + ingresarNombre);
+// }
+
+// Estructura del ciclo for con control de flujo
+// Imprimir todos los números del 0 al 10 menos el número ingresado
+
+// let numeroIngresado = parseInt(prompt("Ingrese Número"));
+// if(isNaN(numeroIngresado)) { // isNaN() verifica si el valor ingresado no es un número el resultado es true sino es false
+//     alert("El valor ingresado no es un número")
+// }
+// for(let i = 0; i <=10; i++) {
+//     if(i != numeroIngresado) {
+//         console.log(i);
+//     }
+// }
+
+// Sentencia Break
+
+// for(let i = 1; i<=10; i++) {
+//     if (i == 3) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// Sentencia Continue
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i == 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
